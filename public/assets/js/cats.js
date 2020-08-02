46e529cc8e93c263b73760cda6d2d7aaec626b81
Allana Anderson <allana.anderson17@gmail.com>
2020-08-02 14:59:29 -0600

    changed all instances of cat to burger in front end cats.js.

--- public/assets/js/cats.js
+++ public/assets/js/cats.js
@@ -1,5 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+  //! DELETE THIS BUTTON
   $(".change-sleep").on("click", function(event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
@@ -9,7 +10,7 @@ $(function() {
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newSleepState
     }).then(
@@ -21,14 +22,14 @@ $(function() {
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  $(".delete-burger").on("click", function(event) {
     event.preventDefault();
 
     var id = $(this).data("id");
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(function() {
-      console.log("Deleted cat with id " + id);
+      console.log("Deleted burger with id " + id);
       location.reload();
     })
   })
@@ -38,17 +39,18 @@ $(function() {
     event.preventDefault();
 
     var newCat = {
+      //!#CA?
       name: $("#ca").val().trim(),
       sleepy: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
