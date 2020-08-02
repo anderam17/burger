a1515440abb01236a6034e98b288bed136061f92
Allana Anderson <allana.anderson17@gmail.com>
2020-08-02 15:11:39 -0600

    bug fixed to make sure all file names and all instances of cat were changed to burger.

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,13 +3,13 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,27 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  //! DELETE THIS BUTTON
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-
-    var newSleepState = {
-      sleepy: newSleep
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
+ 
   $(".delete-burger").on("click", function(event) {
     event.preventDefault();
 
@@ -38,7 +17,7 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       //!#CA?
       name: $("#ca").val().trim(),
       sleepy: $("[name=sleepy]:checked").val().trim()
