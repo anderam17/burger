6700f60eb86e86b6154bb1b5cfd27968bbc12260
Allana Anderson <allana.anderson17@gmail.com>
2020-08-03 20:30:29 -0600

    added jaws config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+
+}
 
 // establish connection
 connection.connect(function(err) {
